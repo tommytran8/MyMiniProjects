d9ffca776442fc1d22cec391a9c5c6a9c93c7aaf
tommytran8 <trantommy71@gmail.com>
2021-03-26 05:10:54 -0700

    More features
    removed intro as start after animation and made profile the new start after animation. Added more graphics

--- site/main.js
+++ site/main.js
@@ -5,13 +5,11 @@ const about = document.getElementById("about");
 const skills = document.getElementById("skills");
 const projects = document.getElementById("projects");
 const arrow = document.getElementById("arrow-container");
+
+const pageslist = [profile,skills,  about, projects];
 window.addEventListener('DOMContentLoaded', () => {
   setTimeout(()=>{
-    document.getElementById("intro-svg").style.display = "none";
-    document.getElementById("intro-text").style.display = "none";
-    document.getElementById("intro-welcome").style.display = "inline";
-    document.getElementById("intro-welcome-before").style.display = "none";
-    intro.style.justifyContent = "center";
+    intro.style.display = "none";
     nav.style.display = "flex";
     profile.style.display = "flex";
     about.style.display = "flex";
@@ -25,29 +23,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener("scroll", ()=>{
   // console.log(intro.scrollHeight, intro.scrollTop, intro.clientHeight, intro.offsetTop, window.screenTop, window.screenY, window.scrollY);
-  if (window.scrollY - (intro.clientHeight+profile.clientHeight+skills.clientHeight+projects.clientHeight)  >= 0){
+  if (window.scrollY - getclientHeight(0, 3) >= 0){
     nav.style.position = "fixed";
     nav.style.transform = "translate(90vw, 5vh)";
-    setNavBgColor("white","white","white","white", "rgb(20, 86, 184)");
-    setNavColor("#0e3e86","#0e3e86","#0e3e86", "#0e3e86", "white");
+    setNavBgColor("white","white","white", "rgb(20, 86, 184)");
+    setNavColor("#0e3e86","#0e3e86", "#0e3e86", "white");
   }
-  else if (window.scrollY - (intro.clientHeight+profile.clientHeight+skills.clientHeight) >= 0){
+  else if (window.scrollY - getclientHeight(0, 2) >= 0){
     nav.style.position = "fixed";
     nav.style.transform = "translate(90vw, 5vh)";
-    setNavBgColor("white","white","white", "rgb(20, 86, 184)","white");
-    setNavColor("#0e3e86","#0e3e86","#0e3e86", "white", "#0e3e86");
+    setNavBgColor("white","white", "rgb(20, 86, 184)","white");
+    setNavColor("#0e3e86","#0e3e86", "white", "#0e3e86");
   }
-  else if (window.scrollY - (intro.clientHeight+profile.clientHeight) >= 0){
+  else if (window.scrollY - getclientHeight(0, 1) >= 0){
     nav.style.position = "fixed";
     nav.style.transform = "translate(90vw, 5vh)";
-    setNavBgColor("white","white", "rgb(20, 86, 184)", "white","white");
-    setNavColor("#0e3e86","#0e3e86", "white", "#0e3e86", "#0e3e86");
-  }
-  else if (window.scrollY - intro.clientHeight >= 0){
-    nav.style.position = "fixed";
-    nav.style.transform = "translate(90vw, 5vh)";
-    setNavBgColor("white", "rgb(20, 86, 184)", "white", "white","white");
-    setNavColor("#0e3e86","white","#0e3e86", "#0e3e86", "#0e3e86");
+    setNavBgColor("white", "rgb(20, 86, 184)", "white","white");
+    setNavColor("#0e3e86", "white", "#0e3e86", "#0e3e86");
   }
   else{
     nav.style.position = "absolute";
@@ -57,20 +49,26 @@ document.addEventListener("scroll", ()=>{
   }
 })
 
-function setNavBgColor(a, b, c, d, e){
-    document.getElementById("to-home").style.backgroundColor = a;
-    document.getElementById("to-profile").style.backgroundColor = b;
+function setNavBgColor(a, b, c, d){
+    document.getElementById("to-profile").style.backgroundColor = a;
+    document.getElementById("to-skills").style.backgroundColor = b;
     document.getElementById("to-about").style.backgroundColor = c;
-    document.getElementById("to-skills").style.backgroundColor = d;
-    document.getElementById("to-projects").style.backgroundColor = e;
+    document.getElementById("to-projects").style.backgroundColor = d;
 }
 
-function setNavColor(a, b, c, d, e){
-  document.getElementById("to-home").style.color = a;
-    document.getElementById("to-profile").style.color = b;
+function setNavColor(a, b, c, d){
+    document.getElementById("to-profile").style.color = a;
+    document.getElementById("to-skills").style.color = b;
     document.getElementById("to-about").style.color = c;
-    document.getElementById("to-skills").style.color = d;
-    document.getElementById("to-projects").style.color = e;
+    document.getElementById("to-projects").style.color = d;
+}
+
+function getclientHeight(x1, x2){
+  let result = 0
+  for (let i = x1; i < x2; i++){
+    result += pageslist[i].clientHeight;
+  }
+  return result;
 }
 
 
